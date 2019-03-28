00856e055b9f42a09b862a6a510680f8fae1c6f1
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-28 14:04:53 -0400

    added parameters to Item model

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,7 +1,20 @@
+using System;
+
 namespace list_api.Models
 {
-    public class Item
-    {
-        
-    }
+  public class Item
+  {
+    public int Id { get; set; }
+
+    public int User_Id { get; set; }
+
+    public string Text { get; set; }
+
+    public bool Complete { get; set; }
+
+    public DateTime Created_At { get; set; }
+
+    public DateTime Updated_At { get; set; }
+
+  }
 }
