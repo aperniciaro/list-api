9f52c808620c8ca9a7247e34206f4d7bdf0dd5f9
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-28 14:05:59 -0400

    migration with item model

---
+++ Migrations/20190328180529_AddedItems.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace list_api.Migrations
+{
+    public partial class AddedItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    User_Id = table.Column<int>(nullable: false),
+                    Text = table.Column<string>(nullable: true),
+                    Complete = table.Column<bool>(nullable: false),
+                    Created_At = table.Column<DateTime>(nullable: false),
+                    Updated_At = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
